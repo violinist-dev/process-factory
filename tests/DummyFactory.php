2574d0fc9685ad92e7a50f31d2b1c12d0f6c664c
Eirik S. Morland <eirik@morland.no>
2019-02-12 16:48:11 +0100

    Code style

--- tests/DummyFactory.php
+++ tests/DummyFactory.php
@@ -7,8 +7,8 @@ use Violinist\ProcessFactory\ProcessFactoryInterface;
 
 class DummyFactory implements ProcessFactoryInterface
 {
-  public function getProcess($commandline, $cwd = null, array $env = null, $input = null, $timeout = 60, array $options = null)
-  {
-      return new Process($commandline, $cwd, $env, $input, $timeout, $options);
-  }
+    public function getProcess($commandline, $cwd = null, array $env = null, $input = null, $timeout = 60, array $options = null)
+    {
+        return new Process($commandline, $cwd, $env, $input, $timeout, $options);
+    }
 }
